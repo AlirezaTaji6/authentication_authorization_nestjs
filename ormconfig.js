11ca73912eec1a9c9863160017738cd3c177a0fd
Alireza Taji <alirezataji6@gmail.com>
2021-08-30 18:53:52 +0430

    feat: add docker

---
+++ ormconfig.js
@@ -0,0 +1,22 @@
+module.exports = {
+   "type": "mysql",
+   "host": "localhost",
+   "port": 3306,
+   "username": "test",
+   "password": "test",
+   "database": "test",
+   "synchronize": true,
+   "logging": false,
+   "entities": [
+      "src/**/*.ts"
+   ],
+   "migrations": [
+      "src/db/migration/**/*.ts"
+   ],
+   "subscribers": [
+      "src/subscriber/**/*.ts"
+   ],
+   "cli": {
+      "migrationsDir": "src/db/migration"
+   }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import "reflect-metadata";
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Appsang project')
+    .setDescription('The appsang API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+    const document = SwaggerModule.createDocument(app, config);
+
+  await app.listen(8000);
 }
 bootstrap();
