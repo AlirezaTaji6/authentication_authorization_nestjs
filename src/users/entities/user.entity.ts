ad498f6991f97c584ae116c8dc9f39d3f5757132
Alireza Taji <alirezataji6@gmail.com>
2021-08-31 09:37:46 +0430

    feat: add user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,45 @@
-import { Entity, Column, PrimaryColumn } from 'typeorm'
+import { hash } from 'bcrypt';
+import { ParentEntity } from 'src/db/entities/parent.entity';
+import { Entity, Column, PrimaryGeneratedColumn, AfterLoad, BeforeInsert, BeforeUpdate } from 'typeorm'
 
 @Entity('users')
-export class UserEntity {
-    @PrimaryColumn({ })
-    id
+export class UserEntity extends ParentEntity {
+    constructor(user?: Partial<UserEntity>) {
+        super();
+
+        this.setArgumentToThisObject(user);
+    }
+
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @Column({ nullable: false, unique: true })
+    phone: string;
+
+    @Column({ nullable: false })
+    password: string;
+
+    private tempPassword: string;
+    @AfterLoad()
+    private loadTempPassword(): void {
+        this.tempPassword = this.password;
+    }
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    async hashPassword() {
+        if(this.password && this.tempPassword !== this.password) {
+            this.password = await hash(this.password, 8);
+        }
+    }
+
+    toJSON() {
+        const result = this;
+        delete result.password;
+        delete result.tempPassword;
+        delete result.created_at;
+        delete result.updated_at;
+        delete result.deleted_at;
+        return result;
+    }
 }
