55a0986f3e00fea4ecb6cb743c55dd30cae13bd1
Alireza Taji <alirezataji6@gmail.com>
2021-08-31 15:10:41 +0430

    chore: first publish

--- src/authorization/authorization.module.ts
+++ src/authorization/authorization.module.ts
@@ -4,10 +4,12 @@ import { AuthorizationController } from './authorization.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RoleEntity } from './entities/role.entity';
 import { PermissionEntity } from './entities/permission.entity';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([RoleEntity, PermissionEntity])
+    TypeOrmModule.forFeature([RoleEntity, PermissionEntity]),
+    UsersModule
   ],
   controllers: [AuthorizationController],
   providers: [AuthorizationService],
